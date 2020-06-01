47be4eb194df6581ce4184e9917dc3acb5214ba6
Edward Mendoza <lalo.garibo90@gmail.com>
2020-06-01 15:33:54 -0700

    Protected user show route

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :set_user, only: [:update, :show, :destroy]
-  before_action :check_owner, only: [:update, :destroy]
+  before_action :set_user, only: [:update, :destroy, :show]
+  before_action :check_owner, only: [:update, :destroy, :show]
 
   def create
     user = User.new(user_params)
