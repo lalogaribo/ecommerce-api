07a38db9e8db0d916272506ee9631fa4585b2399
Edward Mendoza <lalo.garibo90@gmail.com>
2020-06-02 20:11:05 -0700

    Created order model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :products, dependent: :destroy
+  has_many :orders, dependent: :destroy
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :first_name, presence: true
