05755709342ab5775ca73b6051883c5193c3f5e2
Edward Mendoza <lalo.garibo90@gmail.com>
2020-06-04 11:08:57 -0700

    Created mailer to send emil after order creation

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -21,9 +21,10 @@ class Api::V1::OrdersController < ApplicationController
   def create
     order = current_user.orders.build(order_params)
     if order.save
+      OrderMailer.send_confirmation(order).deliver
       render json: order, status: 201
     else
-      render json: { errors: order.errors }, status: 422
+      render json: { errors: order.errors.full_messages }, status: 422
     end
   end
 
