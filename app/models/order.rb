ea9f2f6a5562b9d36919aa3d3cf351f1367b0f42
Edward Mendoza <lalo.garibo90@gmail.com>
2020-06-04 11:40:31 -0700

    Added quantity into placements

--- app/models/order.rb
+++ app/models/order.rb
@@ -13,7 +13,8 @@ class Order < ApplicationRecord
 
   def create_placement(product_ids_and_quantities)
     product_ids_and_quantities.each do |product_id_and_quantity|
-      placement = placements.build(product_id: product_id_and_quantity[:product_id])
+      placement = placements.build(product_id: product_id_and_quantity[:product_id],
+                                   quantity: product_id_and_quantity[:quantity])
       yield placement if block_given?
     end
   end
